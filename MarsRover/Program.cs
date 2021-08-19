65f5c4f164af6c7990a7b00695d5e9f43ca89dd6
halilsavas <58359324+halilsavas@users.noreply.github.com>
2021-08-20 00:15:19 +0300

    Add files via upload

---
+++ MarsRover/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace MarsRover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxPoints = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToList();
+            var startPositions = Console.ReadLine().Trim().Split(' ');
+            Rover position = new Rover();
+
+            if (startPositions.Count() == 3)
+            {
+                position.X = Convert.ToInt32(startPositions[0]);
+                position.Y = Convert.ToInt32(startPositions[1]);
+                position.Direction = startPositions[2];
+            }
+
+            var moves = Console.ReadLine().ToUpper();
+
+            try
+            {
+                position.StartAction(maxPoints, moves);
+                Console.WriteLine($"{position.X} {position.Y} {position.Direction.ToString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MarsRover/Rover.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarsRover
+{
+    public class Rover
+    {
+        public int X;
+        public int Y;
+        public string Direction;
+        public Rover(string location)
+        {
+            Int32.TryParse(location.Split(" ")[0], out X);
+            Int32.TryParse(location.Split(" ")[1], out Y);
+            Direction = location.Split(" ")[2];
+        }
+
+        public Rover()
+        {
+        }
+
+        public void RotateLeft()
+        {
+            switch (Direction)
+            {
+                case "N":
+                    Direction = "W";
+                    break;
+                case "E":
+                    Direction = "N";
+                    break;
+                case "S":
+                    Direction = "E";
+                    break;
+                case "W":
+                    Direction = "S";
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public void RotateRight()
+        {
+            switch (Direction)
+            {
+                case "N":
+                    Direction = "E";
+                    break;
+                case "E":
+                    Direction = "S";
+                    break;
+                case "S":
+                    Direction = "W";
+                    break;
+                case "W":
+                    Direction = "N";
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public void MoveInSameDirection()
+        {
+            switch (Direction)
+            {
+                case "N":
+                    Y += 1;
+                    break;
+                case "E":
+                    X += 1;
+                    break;
+                case "S":
+                    Y -= 1;
+                    break;
+                case "W":
+                    X -= 1;
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public void StartAction(List<int> maxPoints, string moves)
+        {
+            foreach (var move in moves)
+            {
+                switch (move)
+                {
+                    case 'M':
+                        this.MoveInSameDirection();
+                        break;
+                    case 'L':
+                        this.RotateLeft();
+                        break;
+                    case 'R':
+                        this.RotateRight();
+                        break;
+                    default:
+                        Console.WriteLine($"Invalid Character {move}");
+                        break;
+                }
+
+                if (this.X < 0 || this.X > maxPoints[0] || this.Y < 0 || this.Y > maxPoints[1])
+                {
+                    throw new Exception($"Position can not be beyond bounderies (0 , 0) and ({maxPoints[0]} , {maxPoints[1]})");
+                }
+            }
+        }
+    }
+}
